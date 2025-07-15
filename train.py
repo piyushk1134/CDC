fabcdf7b8565c1076d7f8cc541fc127c2e99fe3d
Bluemoon <79992033+piyushk1134@users.noreply.github.com>
2025-07-15 21:08:17 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,61 @@
+import os
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+from tensorflow.keras import layers, models # type: ignore
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+img_size = (128, 128)
+batch_size = 40
+train_dir = 'data/train'
+val_dir = 'data/validation'
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=img_size,
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+val_data = val_datagen.flow_from_directory(
+    val_dir,
+    target_size=img_size,
+    batch_size=batch_size,
+    class_mode='binary'
+)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
+    layers.MaxPooling2D(2, 2),
+
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D(2, 2),
+
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D(2, 2),
+
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+
+history = model.fit(
+    train_data,
+    validation_data=val_data,
+    epochs=24
+)
+
+# Save the model
+model.save('cdm2.keras')
+print("Model saved as cdm2.keras")
